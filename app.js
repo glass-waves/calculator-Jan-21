40083d796b1762637f066021cb33fecaf56ea1a8
Dylan White <dylan.whitej@gmail.com>
2021-01-07 11:57:57 -0800

    adds other operators to calculator

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const input1 = document.getElementById('input-1');
 const input2 = document.getElementById('input-2');
 const addButton = document.getElementById('add-button');
+const subtractButton = document.getElementById('subtract-button');
+const multiplyButton = document.getElementById('multiply-button');
+const divideButton = document.getElementById('divide-button');
 const resultBox = document.getElementById('result-box');
 
 //verification
@@ -13,6 +16,8 @@ const resultBox = document.getElementById('result-box');
 // initialize state
 
 // set event listeners to update state and DOM
+
+//ADD
 addButton.addEventListener('click', () => {
     //verification
     // console.log('button pushed!');
@@ -21,11 +26,43 @@ addButton.addEventListener('click', () => {
     const value2 = input2.valueAsNumber;
 
     const result = value1 + value2;
-
-    //verification
-    console.log(result);
-
     resultBox.textContent = result;
-    
+});
 
-})
+//SUBTRACT
+subtractButton.addEventListener('click', () => {
+    //verification
+    // console.log('button pushed!');
+
+    const value1 = input1.valueAsNumber;
+    const value2 = input2.valueAsNumber;
+
+    const result = value1 - value2;
+    resultBox.textContent = result;
+});
+
+//MULTIPLY
+multiplyButton.addEventListener('click', () => {
+    //verification
+    // console.log('button pushed!');
+
+    const value1 = input1.valueAsNumber;
+    const value2 = input2.valueAsNumber;
+
+    const result = value1 * value2;
+    resultBox.textContent = result;
+});
+
+//DIVIDE
+divideButton.addEventListener('click', () => {
+    //verification
+    // console.log('button pushed!');
+
+    const value1 = input1.valueAsNumber;
+    const value2 = input2.valueAsNumber;
+
+    const result = value1 / value2;
+    resultBox.textContent = result;
+});
+
+    
